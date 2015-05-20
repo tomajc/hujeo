a52004b4a1af28e204ef9c07e9ed20c49d0b627f
tomasz cichowski <szumib@gmail.com>
2015-05-20 10:36:50 +0200

    aaaaaaaaaaa

--- kalkulator.py
+++ kalkulator.py
@@ -1,9 +1,11 @@
 def dodawanie():
-    print("1 liczba")
-    a = int(input())
-    print("2 liczba")
-    b = int(input())
-    print(a+b)
+    return a+b
+
+print("1 liczba")
+a = int(input())
+print("2 liczba")
+b = int(input())
+print(dodawanie(a+b))
 
 
 dodawanie()
