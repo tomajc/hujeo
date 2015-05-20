a649314b5da014c558a2497b560afcd4020b6886
tomasz cichowski <szumib@gmail.com>
2015-05-20 10:24:17 +0200

    nap fun info got do test

--- kalkulator.py
+++ kalkulator.py
@@ -1,9 +1,15 @@
 def dodawanie():
-    print("1 liczba")
-    a = int(input())
-    print("2 liczba")
-    b = int(input())
-    print(a+b)
+    return a+b
 
+print("1 liczba")
+a = int(input())
+print("2 liczba")
+b = int(input())
+print(a+b)
+
+def get_info():
+    print("to jest prost prog kal")
+
+get_info()
 
 dodawanie()
